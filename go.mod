ebc0a7a4db8dbe1a57e2b4f8a191dc96b3b5079a
Peter Svistunov <svistunov.p@i20.team>
2023-11-08 16:25:06 +0800

    Test implementation

---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/mrsad/oidc
+
+go 1.19
+
+require golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+
+require (
+	github.com/go-jose/go-jose/v3 v3.0.1
+	github.com/google/uuid v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/zitadel/oidc/v3 v3.1.1
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/muhlemmer/httpforwarded v0.1.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.4.0 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.19.0 // indirect
+	go.opentelemetry.io/otel/metric v1.19.0 // indirect
+	go.opentelemetry.io/otel/trace v1.19.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/mrsad/oidc/server/config"
+	"github.com/mrsad/oidc/server/op"
+	"github.com/mrsad/oidc/server/storage"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/exp/slog"
+)
+
+func init() {
+	// loads values from .env into the system
+	godotenv.Load()
+}
+
+func main() {
+	cfg := config.New()
+
+	logLevel := slog.LevelInfo
+	if cfg.DebugMode {
+		logLevel = slog.LevelDebug
+	}
+
+	logger := slog.New(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     logLevel,
+		}),
+	)
+
+	issuer := fmt.Sprintf("%s://%s:%s/", cfg.Server.Proto, cfg.Server.Host, cfg.Server.Port)
+
+	// the OpenIDProvider interface needs a Storage interface handling various checks and state manipulations
+	// this might be the layer for accessing your database
+	// in this example it will be handled in-memory
+	storage := storage.NewStorage(storage.NewUserStore(issuer))
+
+	router := op.SetupServer(issuer, storage, logger, false)
+
+	server := &http.Server{
+		Addr:    ":" + cfg.Server.Port,
+		Handler: router,
+	}
+	logger.Info("server listening, press ctrl+c to stop", "addr", issuer)
+	err := server.ListenAndServe()
+	if err != http.ErrServerClosed {
+		logger.Error("server terminated", "error", err)
+		os.Exit(1)
+	}
+}
