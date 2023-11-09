5cb2152646f8b2a69fdec24026f3f04c8cb4dff5
Peter Svistunov <svistunov.p@i20.team>
2023-11-09 12:00:58 +0800

    Code refactoring

--- main.go
+++ main.go
@@ -13,35 +13,28 @@ import (
 	"golang.org/x/exp/slog"
 )
 
+var logger *slog.Logger
+var cfg *config.Config
+
 func init() {
-	// loads values from .env into the system
 	godotenv.Load()
-}
-
-func main() {
-	cfg := config.New()
-
+	cfg = config.New()
 	logLevel := slog.LevelInfo
 	if cfg.DebugMode {
 		logLevel = slog.LevelDebug
 	}
-
-	logger := slog.New(
+	logger = slog.New(
 		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 			AddSource: true,
 			Level:     logLevel,
 		}),
 	)
+}
 
+func main() {
 	issuer := fmt.Sprintf("%s://%s:%s/", cfg.Server.Proto, cfg.Server.Host, cfg.Server.Port)
-
-	// the OpenIDProvider interface needs a Storage interface handling various checks and state manipulations
-	// this might be the layer for accessing your database
-	// in this example it will be handled in-memory
 	storage := storage.NewStorage(storage.NewUserStore(issuer))
-
 	router := op.SetupServer(issuer, storage, logger, false)
-
 	server := &http.Server{
 		Addr:    ":" + cfg.Server.Port,
 		Handler: router,
